891ee6f919bd40cb3cef99aba0a3565a10b76e9e
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-11 02:33:02 +0200

    refactor command update versions

--- cli/update-versions-cmd.go
+++ cli/update-versions-cmd.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"slices"
+	"strings"
 
 	"github.com/Pumahawk/simpl-agents-controller/internal/cmd"
 	"github.com/Pumahawk/simpl-agents-controller/internal/yaml"
@@ -22,17 +24,12 @@ var UpdateVersionCmd = cmd.Cmd{
 		fc.StringVar(&ref, "ref", "main", "")
 		fc.Parse(args)
 
-		type prinfopath struct {
-			authority    []string
-			consumer     []string
-			dataprovider []string
-		}
-
 		readYamlFile := func(ph string) (*yaml.Obj, error) {
 			f, err := os.Open(ph)
 			if err != nil {
 				return nil, fmt.Errorf("unable to open file %q: %w", ph, err)
 			}
+			defer f.Close()
 			bf := &bytes.Buffer{}
 			if _, err := io.Copy(bf, f); err != nil {
 				return nil, err
@@ -41,31 +38,41 @@ var UpdateVersionCmd = cmd.Cmd{
 			return obj, nil
 		}
 
-		authorityFile := "simpl-repo/authority-iaa/charts/values.yaml"
-		authorityObj, err := readYamlFile(authorityFile)
-		if err != nil {
-			return err
-		}
-		consumerFile := "simpl-repo/consumer-iaa/charts/values.yaml"
-		consumerObj, err := readYamlFile(consumerFile)
-		if err != nil {
-			return err
-		}
-		dataproviderFile := "simpl-repo/provider-iaa/charts/values.yaml"
-		dataproviderObj, err := readYamlFile(dataproviderFile)
-		if err != nil {
-			return err
-		}
-
-		var prtoupdate = map[PrInfo]prinfopath{
-			prAuthenticationProvider: {
-				authority: []string{"auth_provider", "targetRevision"},
+		conf := map[string]map[PrInfo][]string{
+			"simpl-repo/authority-iaa/charts/values.yaml": {
+				prAuthenticationProvider: {"auth_provider", "targetRevision"},
 			},
+			"simpl-repo/consumer-iaa/charts/values.yaml": {},
+			"simpl-repo/provider-iaa/charts/values.yaml": {},
 		}
 
-		mapk := make([]PrInfo, 0, len(prtoupdate))
-		for k := range prtoupdate {
-			mapk = append(mapk, k)
+		type yamlSt struct {
+			file string
+			obj  *yaml.Obj
+		}
+		yamlObjs := make(map[string]*yamlSt)
+		for f := range conf {
+			obj, err := readYamlFile(f)
+			if err != nil {
+				return err
+			}
+			yamlObjs[f] = &yamlSt{f, obj}
+		}
+
+		yamlObjFiles := make(map[PrInfo]string)
+		for f, v := range conf {
+			for info := range v {
+				yamlObjFiles[info] = f
+			}
+		}
+
+		mapk := make([]PrInfo, 0)
+		for _, v := range conf {
+			for info := range v {
+				if !slices.Contains(mapk, info) {
+					mapk = append(mapk, info)
+				}
+			}
 		}
 
 		find := make(map[int]bool)
@@ -74,25 +81,31 @@ var UpdateVersionCmd = cmd.Cmd{
 			if !find[v.PrInfo.Id] && v.Ref == ref && !regexp.MustCompile(`\.latest$`).MatchString(v.Version) {
 				find[v.PrInfo.Id] = true
 				v.Stop()
-				path := prtoupdate[v.PrInfo]
-				if path.authority != nil {
-					authorityObj.UpdateAttribute(v.Version, path.authority...)
-				}
-				if path.consumer != nil {
-					consumerObj.UpdateAttribute(v.Version, path.consumer...)
-				}
-				if path.dataprovider != nil {
-					dataproviderObj.UpdateAttribute(v.Version, path.dataprovider...)
+				obj, ok := yamlObjs[yamlObjFiles[v.PrInfo]]
+				if ok {
+					path, ok := conf[obj.file][v.PrInfo]
+					if ok {
+						if _, err := obj.obj.UpdateAttribute(v.Version, path...); err != nil {
+							return fmt.Errorf("unable to update file=%q path=%q", obj.file, strings.Join(path, "."))
+						}
+					}
 				}
 			}
 		}
 
-		authF, err := os.OpenFile(authorityFile, os.O_TRUNC|os.O_WRONLY, 0644)
-		if err != nil {
-			return fmt.Errorf("unable to open file authority %q: %s", authorityFile, err)
-		}
-		if _, err := authF.Write(authorityObj.Bytes()); err != nil {
-			return fmt.Errorf("unable to update file authority %q: %s", authorityFile, err)
+		for file := range conf {
+			fopened, err := os.OpenFile(file, os.O_TRUNC|os.O_WRONLY, 0644)
+			if err != nil {
+				return fmt.Errorf("unable to open file %q: %s", file, err)
+			}
+			defer fopened.Close()
+			for _, obj := range yamlObjs {
+				if obj.file == file {
+					if _, err := fopened.Write(obj.obj.Bytes()); err != nil {
+						return fmt.Errorf("unable to update file=%q: %s", file, err)
+					}
+				}
+			}
 		}
 
 		return nil
