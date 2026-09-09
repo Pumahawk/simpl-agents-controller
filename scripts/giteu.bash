d60ce69885fabdf75319187856311ca58e09e34f
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-09-09 18:11:25 +0200

    udpate out pips from iid to id

--- scripts/giteu.bash
+++ scripts/giteu.bash
@@ -79,7 +79,7 @@ function git_get_project_id() {
 
 function pipelines() {
   prid="$(git_get_project_id)"
-  c_giteu "projects/$prid/pipelines" -G "$@" | apiout -r '.[] | "\(.project_id) \(.iid) \(.status) \(.ref) \(.created_at) \(.updated_at) \(.web_url)"'
+  c_giteu "projects/$prid/pipelines" -G "$@" | apiout -r '.[] | "\(.project_id) \(.id) \(.status) \(.ref) \(.created_at) \(.updated_at) \(.web_url)"'
 }
 
 function merge_requests() {
